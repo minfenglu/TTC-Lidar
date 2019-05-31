b77c7c08568a0576bb3e778410a9e8f783db2916
Minfeng Lu <mlu@zoox.com>
2019-05-31 16:42:34 -0700

    Skeleton code set up

---
+++ src/compute_ttc_lidar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <numeric>
+#include <opencv2/core.hpp>
+
+
+#include "dataStructures.h"
+#include "structIO.hpp"
+
+using namespace std;
+int main(){
+    return 0;
+}
